8257178743d984c97c5a60366233864e5be7db94
Rustam <Hec8832@gmail.com>
2023-06-16 00:10:25 +0600

    работа в семинаре

---
+++ exmp001/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите целое число: ");
+string userInput = Console.ReadLine() ??"";
+int userNumber = int.Parse(userInput);
+
+int result = userNumber*userNumber;
+
+Console.WriteLine($" {userNumber} -> {result} ");
---
+++ exmp002/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите первое целое число: ");
+string userImput1 = Console.ReadLine() ?? "";
+int a = int.Parse(userImput1);
+
+Console.Write("Введите второе целое число: ");
+string userImput2 = Console.ReadLine() ?? "";
+int b = int.Parse(userImput2);
+
+if (a == b * b)
+{
+    Console.WriteLine($" a = {a} , b={b} -> Да");
+}
+else
+{
+    Console.WriteLine($" a = {a} , b={b} -> Нет");
+}
+
